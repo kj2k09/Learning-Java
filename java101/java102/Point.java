5560cf9cca22bbab255cc0ff4aed324eabbf7901
kj2k09 <kj2k09@gmail.com>
2024-11-21 22:25:02 -0500

    Update Point.java

--- java101/java102/Point.java
+++ java101/java102/Point.java
@@ -1,1 +1,34 @@
+public class Point {
+    public double x; 
+    public double y;
+    
+    public Point(double x,  double y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    public static Point centerOfMass(Point[] points) {
+        double sumx = 0;
+        double sumy = 0;
+        
+        for (Point point: points) {
+            sumx += point.x;
+            sumy += point.y;
+        }
+        
+        return new Point(sumx / points.length, sumy / points.length);
+    }
+    
+    // Main method for testing
+    public static void main(String[] args) {
+        Point[] points = {
+            new Point(1,  2), 
+            new Point(3,  4), 
+            new Point(5,  6)
+        };
+
+        Point center = Point.centerOfMass(points);
+        System.out.println("Center of Mass:  " + center);
+    }
+}
 
